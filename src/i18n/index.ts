f23235b24ceddb3e54e1e5bda026bf8f9b9fa83c
AliMohammadDev <aloshmohammad2001@gmail.com>
2026-07-02 14:09:23 +0300

    refactor in i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -15,8 +15,8 @@ i18n
     },
     fallbackLng: "en",
     supportedLngs: ["en", "ar"],
-    load: "languageOnly", // 💡 تجبر i18next على تنظيف الرموز مثل en-US لتصبح en فقط
-    returnObjects: true,  // تفعيل جلب المصفوفات والكائنات في التطبيق بالكامل
+    load: "languageOnly",
+    returnObjects: true,
     interpolation: {
       escapeValue: false,
     },
